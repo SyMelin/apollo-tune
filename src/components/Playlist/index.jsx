6917770ef6d5c3e3fe2b93f7d69d9703844f7174
Sylvie Melin <sy.melin@gmail.com>
2023-02-02 21:10:16 +0100

    Playlist

--- src/components/Playlist/index.jsx
+++ src/components/Playlist/index.jsx
@@ -1,7 +1,49 @@
+import { Delete } from "@mui/icons-material"
+import { Avatar, Typography, IconButton } from "@mui/material"
+
+function AwaitingSong({ song }) {
+    const { thumbnail, artist, title } = song
+    return (
+        <div style={{
+            display: 'grid',
+            gridAutoFlow: 'column',
+            gridTemplateColumns: '50px auto 50px',
+            gap: '12px',
+            alignItems: 'center',
+            marginTop: '10px'
+        }}>
+            <Avatar sx={{width: '44px', height: '44px'}} src={thumbnail} alt="Song thumbnail" />
+            <div style={{ overflow: 'hidden', whiteSpace: 'nowrap'}}>
+                <Typography sx={{textOverflow: 'ellipsis', overflow: 'hidden'}} variant="subtitle2">
+                    {title}
+                </Typography>
+                <Typography sx={{textOverflow: 'ellipsis', overflow: 'hidden'}} color="textSecondary" variant="body2">
+                    {artist}
+                </Typography>
+            </div>
+            <IconButton>
+                <Delete color="error" />
+            </IconButton>
+        </div>
+    )
+}
+
 function Playlist() {
+    const song = {
+        title: "LÜNE",
+        artist: "MÖÖN",
+        thumbnail: "http://img.youtube.com/vi/--ZtUFsIgMk/0.jpg"
+    }
 
     return (
-        <div>Playlist</div>
+        <div style={{ margin: '10px 0' }}>
+            <Typography color="textSecondary" variant="button">
+                PLAYLIST(5)
+            </Typography>
+            {Array.from({ length: 5 }, () => song).map((song, index) => (
+                <AwaitingSong key={index} song={song} />
+            ))}
+        </div>
     )
 }
 
