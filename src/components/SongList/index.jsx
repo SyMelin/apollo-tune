0e615162bc6e0421bb84d3c29041602fed3dbd0e
Sylvie Melin <sy.melin@gmail.com>
2023-02-13 18:39:58 +0100

    sync of Song play button and SongPlayer play button

--- src/components/SongList/index.jsx
+++ src/components/SongList/index.jsx
@@ -1,10 +1,20 @@
 import { useSubscription } from "@apollo/client"
-import { PlayArrow, Save } from "@mui/icons-material"
+import { Pause, PlayArrow, Save } from "@mui/icons-material"
 import { Card, CardActions, CardContent, CardMedia, CircularProgress, IconButton, Typography } from "@mui/material"
+import { useContext, useEffect, useState } from "react"
 import { GET_SONGS } from "../../graphql/subscriptions"
+import { SongContext } from "../App"
 
 function Song({ song }) {
+    const { id } = song
+    const { state } = useContext(SongContext)
+    const [currentSongPlaying, setCurrentSongPlaying] = useState(false)
     const { title, artist, thumbnail } = song
+
+    useEffect(() => {
+        const isSongPlaying = state.isPlaying && id === state.song.id
+        setCurrentSongPlaying(isSongPlaying)
+    }, [id, state.song.id, state.isPlaying])
 
     return (
         <Card sx={{ margin: "36px" }}>
@@ -21,7 +31,10 @@ function Song({ song }) {
                     </CardContent>
                     <CardActions>
                         <IconButton size="small" color="primary">
-                            <PlayArrow />
+                            { currentSongPlaying
+                            ? <Pause />
+                            :<PlayArrow />
+                            }
                         </IconButton>
                         <IconButton size="small" color="secondary">
                             <Save />
